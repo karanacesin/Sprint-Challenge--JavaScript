b971f295464c238c2c02201fca6f99a944fcb6a7
Dawna Backmeier <karanacesin@yahoo.com>
2020-04-24 13:11:53 -0400

    final

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,28 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+class CuboidMaker2{
+
+    constructor(l,w,h){
+        this.l = l; //length
+        this.w = w; //width
+        this.h = h; //height
+    }
+
+    volume(){
+        return this.l * this.w * this.h;
+      };
+
+    surfaceArea(){
+        return 2 * (this.l * this.w + this.l * this.h + this.w * this.h)
+    };
+}
+
+const cuboid2 = new CuboidMaker2(4,5,5);
+
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+ console.log(cuboid2.volume()); // 100
+ console.log(cuboid2.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,20 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation:
 
+
+  /* when a function is called if the variable is not inside the function it will look outside of the function going out one by one until it finds the variable*/
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+const factorial = function(num){
+
+  for( let i = num - 1; i >= 1; i--){
+
+    num += i;
+  }
+  return num;
+}
